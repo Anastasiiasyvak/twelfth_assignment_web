4bdb61bb697480f0dfb78660d24e0c907e8643a9
Anastasiiasyvak <asyvak@kse.org.ua>
2023-10-18 16:49:26 +0300

    created react project'

---
+++ week-planner/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Week from './Week'; 
+
+const App = () => {
+  return (
+    <div className="app">
+      <Week /> 
+    </div>
+  );
+}
+
+export default App;
---
+++ week-planner/src/Week.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Week = () => {
+  return (
+    <div className="week">
+      <h2>My Weekly Planner</h2>
+    
+    </div>
+  );
+}
+
+export default Week
